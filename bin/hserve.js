b4a887e60325a3de79cfeebb4475765b44df9a23
bagaking <zfaustk@qq.com>
2018-11-08 05:37:51 +0800

    feat: mock commond defined; cmd -d indicates relative path now.

--- bin/hserve.js
+++ bin/hserve.js
@@ -18,8 +18,15 @@ var argv = Argv
     .option('d', {
     alias: 'dir',
     demand: false,
+    default: '.',
+    describe: 'relative path to serve',
+    type: 'string'
+})
+    .option('m', {
+    alias: 'mock',
+    demand: false,
     default: null,
-    describe: 'static path to serve',
+    describe: 'relative path to mock',
     type: 'string'
 })
     .option('p', {
@@ -29,19 +36,29 @@ var argv = Argv
     describe: 'port the service on',
     type: 'string'
 })
-    .usage('Usage: hserve [options]')
-    .example('hserve -d /var/www/my_blog -p 80', 'serve the folder "my_blog" at port 80.')
+    .usage('Usage: hserve PATH [OPTIONS]')
+    .example('hserve /var/www/my_blog', 'serve the folder "my_blog" at port 3000.')
+    .example('hserve /var/www -d my_blog -p 80', 'serve the folder "my_blog" at port 80.')
     .help('h').alias("h", "help")
     .epilog('Copyright 2018')
     .argv;
 var exp = require('express');
 var app = exp();
 var port = argv.port;
-var path = argv.dir ? (path_1.default.isAbsolute(argv.dir)
-    ? argv.dir
-    : path_1.default.join(process.cwd(), argv.dir))
-    : process.cwd();
-app.use("/", express.static(path));
+var root = argv._[0];
+var rootPath = root ? (path_1.default.isAbsolute(root) ? root : path_1.default.join(process.cwd(), root)) : process.cwd();
+var servePath = path_1.default.join(rootPath, argv.dir);
+app.use("/", express.static(servePath));
+var mockPath;
+if (!!argv.mock) {
+    mockPath = path_1.default.join(rootPath, argv.mock);
+}
+console.log(argv.mock);
 app.listen(port, function () {
-    console.log("\n**** Server Start ****\nServe path : " + path + " \nListening at : http://localhost:" + port + "/\n**********************\n");
+    console.log('**** Service Preparing ****\n');
+    console.log("- Root path : " + rootPath);
+    console.log("\n- Serve : \n    - path : " + servePath + " \n    - at : http://localhost:" + port + "/\n");
+    console.log(!mockPath ? '- Mock : off\n' :
+        "\n- Mock :\n    - path : " + mockPath + "\n    - at : http://localhost:" + port + "/api/\n");
+    console.log('**** Service Running ******\n');
 });
--- hserve.ts
+++ hserve.ts
@@ -3,13 +3,21 @@
 import * as express from 'express';
 import * as Argv from 'yargs'
 import Path from 'path';
+import fs from 'fs';
 
 const argv = Argv
     .option('d', {
         alias: 'dir',
         demand: false,
+        default: '.',
+        describe: 'relative path to serve',
+        type: 'string'
+    })
+    .option('m', {
+        alias: 'mock',
+        demand: false,
         default: null,
-        describe: 'static path to serve',
+        describe: 'relative path to mock',
         type: 'string'
     })
     .option('p', {
@@ -19,8 +27,9 @@ const argv = Argv
         describe: 'port the service on',
         type: 'string'
     })
-    .usage('Usage: hserve [options]')
-    .example('hserve -d /var/www/my_blog -p 80', 'serve the folder "my_blog" at port 80.')
+    .usage('Usage: hserve PATH [OPTIONS]')
+    .example('hserve /var/www/my_blog', 'serve the folder "my_blog" at port 3000.')
+    .example('hserve /var/www -d my_blog -p 80', 'serve the folder "my_blog" at port 80.')
     .help('h').alias("h", "help")
     .epilog('Copyright 2018')
     .argv;
@@ -29,21 +38,38 @@ const exp = require('express')
 
 const app: express.Application = exp();
 const port: number = argv.port;
-const path: string =
-    argv.dir ? (Path.isAbsolute(argv.dir)
-        ? argv.dir
-        : Path.join(process.cwd(), argv.dir))
-        : process.cwd();
+const root : string = argv._[0];
 
-app.use("/", express.static(path));
+const rootPath: string = root ? (Path.isAbsolute(root) ? root : Path.join(process.cwd(), root)) : process.cwd();
+const servePath = Path.join(rootPath, argv.dir);
+
+app.use("/", express.static(servePath));
+
+let mockPath : string;
+if(!!argv.mock){
+    mockPath = Path.join(rootPath, argv.mock);
+
+
+
+}
+console.log(argv.mock)
+
+
 app.listen(port, () => {
-    console.log(
-        `
-**** Server Start ****
-Serve path : ${path} 
-Listening at : http://localhost:${port}/
-**********************
+    console.log('**** Service Preparing ****\n');
+    console.log(`- Root path : ${rootPath}`);
+    console.log(`
+- Serve : 
+    - path : ${servePath} 
+    - at : http://localhost:${port}/
+`);
+    console.log(!mockPath ? '- Mock : off\n' :
 `
-    );
+- Mock :
+    - path : ${mockPath}
+    - at : http://localhost:${port}/api/
+`)
+    console.log('**** Service Running ******\n');
 });
+
 
